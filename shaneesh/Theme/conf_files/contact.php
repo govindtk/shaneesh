b19929a3483e4a2c8ce81772b29cfb2cfb8d9df7
Govind <govind405036@gmail.com>
2017-05-11 21:22:16 +0530

    first commit

---
+++ shaneesh/Theme/conf_files/contact.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$name=$_POST['val'];
+echo "Message sent successfully...".$name;
+//$email=$_POST['email'];
+//$subject=$_POST['subject'];
+//$comments=$_POST['comments'];
+// $retval = mail ('mshaneesh@gmail.com',$subject,$comments,$name);
+//         
+//         if( $retval == true ) {
+//            echo "Message sent successfully...";
+//         }else {
+//            echo "Message could not be sent...";
+//         }
+?>
